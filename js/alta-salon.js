a5bf441fe082fc7538fe6c71776bd5b64d7c5ff4
Santiago Bertorello <santinbertorello@gmail.com>
2025-05-26 23:06:26 -0300

    Inicio js, se capturan selectores.
    Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>
    Co-authored-by: Gabr!ela. <gabrielasan07@gmail.com>
    Co-authored-by: Estreyani <Estreyani@users.noreply.github.com>

---
+++ js/alta-salon.js
@@ -0,0 +1,20 @@
+//#boton-agregar
+
+const botonAgregar = document.GetElementById('boton-agregar');
+
+//boton-agregarSalon del formulario
+const btnAgregarSalon = document.querySelectorAll('#btn-agregarSalon');
+
+//boton-agregarSalon eliminarSalon
+const botonEditar = document.querySelector("#boton-editar");
+const btnEliminar = document.querySelector("#boton-eliminar")
+
+
+
+
+function desplegarFormulario(){};
+
+
+function agregarSalon(){
+    
+}
