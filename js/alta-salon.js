d0cdeb9f021482b589a7ce7b33aa2717eedd0c0f
Santiago Bertorello <santinbertorello@gmail.com>
2025-05-27 00:13:05 -0300

    add: js actualizacion form
    Co-authored-by: Victoria Inés Avalos <vickkyavalos@users.noreply.github.com>
    Co-authored-by: Estreyani <Estreyani@users.noreply.github.com>
    Co-authored-by: Gabr!ela. <gabrielasan07@gmail.com>

--- js/alta-salon.js
+++ js/alta-salon.js
@@ -1,28 +1,46 @@
 //#boton-agregar
 
 const btnDesplegarFormulario = document.getElementById('btn-desplegarFormulario');
-const element = document.getElementById('form-admin');
+const formAdmin = document.getElementById('form-admin');
 
 
 //boton-agregarSalon del formulario
-const btnagregarSalon = document.querySelectorAll('#btn-agregarSalon');
+const btnAgregarSalon = document.querySelectorAll('#btn-agregarSalon');
 const formulario = document.querySelectorAll('#formulario')
 
 //boton-agregarSalon eliminarSalon
 const botonEditar = document.querySelector("#boton-editar");
-const btnEliminar = document.querySelector("#boton-eliminar")
+const btnEliminar = document.querySelector("#boton-eliminar");
+
+// id de inputs de formulario (nombre, direccion, descripcion, precio)
+const tituloform = document.getElementById('titulo-form');
+const descripcion = document.getElementById('inputDescripcionSalon')
+const direccionSalon= document.getElementById('inputDireccion');
+const precioSalon = document.getElementById('inputPrecio');
 
 
-btnDesplegarFormulario.addEventListener('click', function() {
+const salones = [
+  { title: ""}
+];
+
+btnDesplegarFormulario.addEventListener('click', function(event) {
+    event.preventDefault();
     desplegarFormulario()
+});
+
+btnAgregarSalon.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    
 });
 
 
 function desplegarFormulario(){
     if (formAdmin.style.visibility === 'hidden') {
       formAdmin.style.visibility = 'visible';
+
     } else {
-      elforAdmintyle.visibility = 'hidden';
+      formAdmin.style.visibility = 'hidden';
     }
 };
 
@@ -30,3 +48,5 @@ function desplegarFormulario(){
 function agregarSalon(){
 
 }
+
+
