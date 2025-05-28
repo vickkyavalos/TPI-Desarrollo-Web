2d2c004282ad1924b6f4fff33413a51f6ef08303
Victoria Ines <inessavalos14@gmail.com>
2025-05-28 01:02:00 -0300

    fix actualizacion login
    Co-authored-by: Gabr!ela. <gabrielasan07@gmail.com>
    Co-authored-by: Noah Cassutti <noahcassutti@users.noreply.github.com>
    Co-authored-by: Estreyani <Estreyani@users.noreply.github.com>
    Co-authored-by: Santiago <SantiagoBertorello@users.noreply.github.com>

---
+++ js/login.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("btn");
+const container = document.querySelector(".container-login");
+
+btn.addEventListener("click",()=>{
+   container.classList.toogle("toggle");
+});
