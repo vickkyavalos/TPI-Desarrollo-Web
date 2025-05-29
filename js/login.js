a330e59ea17d041e172528b524e42488cb811bf0
¨Estrellagit config --global user.email estreyani@gmail.com¨ <Estreyani@gmail.com>
2025-05-29 08:49:15 -0300

    se realiza cambios en login

---
+++ js/login.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container-login");
+const btnregistra = document.getElementById("btn-registra");
+const btninicia = document.getElementById("btn-inicia");
+
+btnregistra.addEventListener("click", () => {
+  container.classList.add("toggle");
+});
+
+btninicia.addEventListener("click", () => {
+  container.classList.remove("toggle");
+});
+
